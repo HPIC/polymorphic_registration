d33c89d26a2c8443ce81939b9b32c07f43d73dcd
rnilva <rnilva849@gmail.com>
2023-11-23 10:33:37 +0900

    Add ModuleGetter

---
+++ module_getter.py
@@ -0,0 +1,43 @@
+import inspect
+import importlib
+from pathlib import Path
+from typing import Generic, TypeVar, Type, Literal
+
+
+T = TypeVar('T')
+
+class ModuleGetter(Generic[T]):
+    def __init__(self, module_name: str, type: Type[T]) -> None:
+        super().__init__()
+        self._m_name = module_name
+        self._t = type
+        self._dict: dict[str, Type[T]] = {}
+        
+        submodule_path = Path(module_name)
+        if not submodule_path.exists():
+            raise ValueError(f"Module {module_name} does not exist.")
+
+        for f in submodule_path.iterdir():
+            if f.suffix != ".py": continue
+            module = importlib.import_module(f"{module_name}.{f.stem}")
+
+            for name, cls in inspect.getmembers(module):
+                if inspect.isclass(cls) and issubclass(cls, type):
+                    self._dict[name] = cls
+    def get(self, name: str, **kwargs) -> T:
+        cls = self._dict.get(name)
+        if not cls:
+            raise ValueError(f"Invalid module name {name}")
+        
+        return cls(**kwargs)
+
+
+def test():
+    import algorithm
+
+    m = ModuleGetter("algorithms", algorithm.IAlgorithm)
+    assert issubclass(type(m.get("Algo1")), algorithm.IAlgorithm)
+
+
+if __name__ == "__main__":
+    test()
